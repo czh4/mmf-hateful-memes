2c6a97913fe524a3c62d85208c1f302d057202a6
Andy <45130444+czh4@users.noreply.github.com>
2020-10-23 09:31:40 -0400

    Update download.py

--- mmf/utils/download.py
+++ mmf/utils/download.py
@@ -214,9 +214,8 @@ def download(url, path, fname, redownload=True, disable_tqdm=False):
     Returns whether download actually happened or not
     """
     outfile = os.path.join(path, fname)
-    #download = not PathManager.isfile(outfile) or redownload
-    # don't download features
-    download = False
+    print(outfile)
+    download = not PathManager.isfile(outfile) or redownload
     retry = 5
     exp_backoff = [2 ** r for r in reversed(range(retry))]
 
